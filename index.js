dd8117d035b805c3a41c56918b9606c41bfdcff5
aarush verma <aarushverma123@gmail.com>
2020-07-28 21:06:29 +0530

    final

--- index.js
+++ index.js
@@ -2,10 +2,35 @@
 const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
-
 const path = require('path');
+const request = require('request');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
+
+
+
+//body parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+
+//api key pk_1eb1b38f273342cfb3eb243a68aae116
+//create call api function
+
+function call_api(finishedAPI,ticker) {
+    request('https://cloud.iexapis.com/stable/stock/'+ ticker + '/quote?token=pk_1eb1b38f273342cfb3eb243a68aae116', { json: true }, (err, res, body) => {
+  if(err){return console.log(err);}
+
+  if(res.statusCode === 200){
+      //console.log(body);
+
+      finishedAPI(body);
+
+  };
+
+});
+
+}
+ 
 
 //set handele bars
 
@@ -14,12 +39,33 @@ app.set('view engine', 'handlebars');
 
 const otherstuff = "this is other stuff";
 
-//set handlebars routes
+//set handlebars index GET routes
 app.get('/', function (req, res) {
-    res.render('home',{
-        stuff: otherstuff
-    });
+	call_api(function(doneAPI) {
+			res.render('home', {
+	    	stock: doneAPI,
+    	});
+	}, "fb");
+		
 });
+
+//set handlebars index POST routes
+app.post('/', function (req, res) {
+	call_api(function(doneAPI) {
+			//console.log(doneAPI);
+			//posted_stuff = req.body.stock_ticker;
+			res.render('home', {
+	    	stock: doneAPI,
+    	});
+	}, req.body.stock_ticker);
+		
+});
+
+//create about page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
+});
+
 
 // set static folder
 
